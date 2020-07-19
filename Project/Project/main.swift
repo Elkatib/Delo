8c72b66f917b877cba504de5b0acbfe863adda51
Elkatib <alkatib.ru@gmail.com>
2020-07-19 14:32:42 +0300

    2 vetka zadacha 2

--- Project/Project/main.swift
+++ Project/Project/main.swift
@@ -9,23 +9,11 @@
 import Foundation
 
 
-var x1: Double = 0
-var x2: Double = 0
-var D: Double = 0
- 
-let a: Double = 2
-let b: Double = -9
-let c: Double = 4
- 
-D = b * b - 4 * a * c
- 
-if D > 0 {
-    x1 = (-b + sqrt(D)) / (2 * a)
-    x2 = (-b - sqrt(D)) / (2 * a)
-    print("X1 = \(x1); X2 = \(x2)")
-} else if D == 0 {
-    x1 = (-b + sqrt(D)) / (2 * a)
-    print("X1 = X2 = \(x1)")
-} else {
-    print("Невозможно")
-}
+var side1:Double = 6
+var side2:Double = 8
+let gypo:Double = side1*side2 + side2*side2
+let s:Double = (side1*side2)/2
+let p:Double = (side1 + side2 + gypo)
+print("Гипотенуза: \(gypo)")
+print("Площадь треугольника: \(s)")
+print("Периметр треугольника: \(p)")
