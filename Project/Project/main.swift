2f4aa2c34ee117521310f7d6cd1fd271f597ef8c
Elkatib <alkatib.ru@gmail.com>
2020-07-19 14:42:56 +0300

    Dobail 3 zadanie

--- Project/Project/main.swift
+++ Project/Project/main.swift
@@ -9,23 +9,13 @@
 import Foundation
 
 
-var x1: Double = 0
-var x2: Double = 0
-var D: Double = 0
- 
-let a: Double = 2
-let b: Double = -9
-let c: Double = 4
- 
-D = b * b - 4 * a * c
- 
-if D > 0 {
-    x1 = (-b + sqrt(D)) / (2 * a)
-    x2 = (-b - sqrt(D)) / (2 * a)
-    print("X1 = \(x1); X2 = \(x2)")
-} else if D == 0 {
-    x1 = (-b + sqrt(D)) / (2 * a)
-    print("X1 = X2 = \(x1)")
-} else {
-    print("Невозможно")
+var sum:Double = 1000
+var perc:Double = 5.7
+var i = 1
+
+while i < 6 {
+    i += 1
+    sum = sum + (sum * perc) / 100
 }
+
+print("Через пять лет сумма вклада составит:  \(round(sum))")
