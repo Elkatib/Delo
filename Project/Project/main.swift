e5059ec296041d93b25bc1103776eb6ce3671c1c
Elkatib <alkatib.ru@gmail.com>
2020-07-19 14:10:19 +0300

    Dobavil Pervi Urok

--- Project/Project/main.swift
+++ Project/Project/main.swift
@@ -8,5 +8,24 @@
 
 import Foundation
 
-print("Hello, World!")
 
+var x1: Double = 0
+var x2: Double = 0
+var D: Double = 0
+ 
+let a: Double = 2
+let b: Double = -9
+let c: Double = 4
+ 
+D = b * b - 4 * a * c
+ 
+if D > 0 {
+    x1 = (-b + sqrt(D)) / (2 * a)
+    x2 = (-b - sqrt(D)) / (2 * a)
+    print("X1 = \(x1); X2 = \(x2)")
+} else if D == 0 {
+    x1 = (-b + sqrt(D)) / (2 * a)
+    print("X1 = X2 = \(x1)")
+} else {
+    print("Невозможно")
+}
