b8c3174081990512f56570c01240b4183be19c23
Elkatib <alkatib.ru@gmail.com>
2020-07-20 20:06:11 +0300

    add Lesson3 main.swift

--- Project/Project/main.swift
+++ Project/Project/main.swift
@@ -8,24 +8,38 @@
 
 import Foundation
 
+var numbers: [Int] = fillArray()
+var numbersWhithout: [Int] = deleteSomeNumbers(numbers: numbers)
 
-var x1: Double = 0
-var x2: Double = 0
-var D: Double = 0
- 
-let a: Double = 2
-let b: Double = -9
-let c: Double = 4
- 
-D = b * b - 4 * a * c
- 
-if D > 0 {
-    x1 = (-b + sqrt(D)) / (2 * a)
-    x2 = (-b - sqrt(D)) / (2 * a)
-    print("X1 = \(x1); X2 = \(x2)")
-} else if D == 0 {
-    x1 = (-b + sqrt(D)) / (2 * a)
-    print("X1 = X2 = \(x1)")
-} else {
-    print("Невозможно")
+func even(number:Int) -> Bool { // Четное число или нет
+    let funcAnswer:Bool = number % 2 == 0 ? true : false
+    return funcAnswer
 }
+
+func evenForThree(number:Int) -> Bool { //Делится на три без остатка или нет
+    let funcAnswer:Bool = number % 3 == 0 ? true : false
+    return funcAnswer
+}
+
+func fillArray() -> [Int] { //Наполняем массив числами через цикл
+    var temporaryNnumbers: [Int] = [] //Вводим промежуточный массив, так как параметр по ходу в Сфивте это константа
+    for i in 1...100{
+        temporaryNnumbers.append(i)
+    }
+    return temporaryNnumbers
+}
+
+func deleteSomeNumbers(numbers:[Int]) -> [Int]{
+    var temporaryNumbers: [Int] = numbers
+    var i = 0
+    while i < temporaryNumbers.count {
+        if even(number: temporaryNumbers[i]) == true || evenForThree(number: temporaryNumbers[i]) == false{
+            temporaryNumbers.remove(at: i)
+            //Не производим итерацию, так как массив уменьшился и под элементом i в массиве лежит уже следующая цифра
+        } else {
+            i += 1
+        }
+    }
+    return temporaryNumbers
+}
+
