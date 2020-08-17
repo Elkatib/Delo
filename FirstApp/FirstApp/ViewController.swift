1a4df64484ebd89e7b64ad10a02f618b02ba0f58
Elkatib <alkatib.ru@gmail.com>
2020-08-17 13:27:23 +0300

    Add GUI1

---
+++ FirstApp/FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by gt650 on 15.08.2020.
+//  Copyright © 2020 gt650. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
