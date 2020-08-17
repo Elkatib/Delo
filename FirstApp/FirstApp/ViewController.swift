24bd19965638060de93d61f7321303e87176aafc
Elkatib <alkatib.ru@gmail.com>
2020-08-17 13:27:52 +0300

    Add GUI1

--- FirstApp/FirstApp/ViewController.swift
+++ FirstApp/FirstApp/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var login: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        let log = login.text
+        let pas = password.text
+        
+        if log == "admin" && pas == "1234" {
+            print("Вход выполнен")
+        }
+    }
 }
 
